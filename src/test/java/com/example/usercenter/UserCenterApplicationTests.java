afd832a405bc762094e9bbb30f25d09edaf7905c
Ghost <2089049079@qq.com>
2023-12-02 12:07:56 +0800

    add 用户注册业务代码

--- src/test/java/com/example/usercenter/UserCenterApplicationTests.java
+++ src/test/java/com/example/usercenter/UserCenterApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.usercenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class UserCenterApplicationTests {
@@ -9,5 +10,15 @@ class UserCenterApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    /**
+     * 测试加密算法
+     */
+    @Test
+    void testDigest() {
+        String md5DigestAsHex = DigestUtils.md5DigestAsHex(("GHOST" + "myPassword").getBytes());
+        System.out.println(md5DigestAsHex);
+    }
+
 
 }
